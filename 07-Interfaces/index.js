0fefb07b611325789b7a867e30301a1282029736
Zeh-Eox <arnolddevmail@gmail.com>
2025-03-18 21:08:36 +0000

    Interface

---
+++ 07-Interfaces/index.js
@@ -0,0 +1,14 @@
+"use strict";
+/**
+ * Les interfaces en TypeScript sont utilisées pour définir la structure d'un objet.
+ * Elles permettent de spécifier les propriétés et leurs types, ainsi que de garantir
+ * que les objets respectent cette structure. Les interfaces peuvent également être
+ * étendues pour créer des structures plus complexes.
+ */
+const personnage = {
+    nom: 'John',
+    age: 30,
+    sexe: 'homme'
+};
+console.log(personnage); // { nom: 'John', age: 30, sexe: 'homme' }
+console.log(typeof (personnage)); // object
---
+++ 07-Interfaces/index.ts
@@ -0,0 +1,21 @@
+/**
+ * Les interfaces en TypeScript sont utilisées pour définir la structure d'un objet.
+ * Elles permettent de spécifier les propriétés et leurs types, ainsi que de garantir
+ * que les objets respectent cette structure. Les interfaces peuvent également être
+ * étendues pour créer des structures plus complexes.
+ */
+
+interface Personnage {
+    nom: string;
+    age: number;
+    sexe: string;
+}
+
+const personnage: Personnage = {
+    nom: 'John',
+    age: 30,
+    sexe: 'homme'
+};
+
+console.log(personnage); // { nom: 'John', age: 30, sexe: 'homme' }
+console.log(typeof(personnage)); // object
